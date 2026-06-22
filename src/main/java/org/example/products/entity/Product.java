f02125ec2460e92fedf6667831de9efa1b464ed9
hugo.bonete.alonso <hugo.bonete.alonso@accenture.com>
2026-06-22 11:36:15 +0200

    fixed createAt date not assigned when created

--- src/main/java/org/example/products/entity/Product.java
+++ src/main/java/org/example/products/entity/Product.java
@@ -7,11 +7,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @AllArgsConstructor
--- src/main/java/org/example/products/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/products/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.example.products.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -19,35 +20,40 @@ public class GlobalExceptionHandler {
     private static final String PATH = "path";
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex
+    , HttpServletRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put(TIMESTAMP, LocalDateTime.now());
         body.put(STATUS, 400);
         body.put(ERROR, "BAD_REQUEST");
         body.put(MESSAGE, ex.getMessage());
-        body.put(PATH, ex.getNestedPath());
+        body.put(PATH, request.getRequestURI());
 
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleGenericException(Exception ex) {
+    public ResponseEntity<Object> handleGenericException(Exception ex,
+     HttpServletRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put(TIMESTAMP, LocalDateTime.now());
         body.put(STATUS, 500);
         body.put(ERROR, "INTERNAL_SERVER_ERROR");
         body.put(MESSAGE, ex.getMessage());
+        body.put(PATH, request.getRequestURI());
 
         return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException ex) {
+    public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException ex,
+     HttpServletRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put(TIMESTAMP, LocalDateTime.now());
         body.put(STATUS, 404);
         body.put(ERROR, "PRODUCT_NOT_FOUND");
         body.put(MESSAGE, ex.getMessage());
+        body.put(PATH, request.getRequestURI());
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
